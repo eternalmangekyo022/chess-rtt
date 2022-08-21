cf41aff85e057bd94daa90ede34a5f54ea005990
hxrd <tibcso0322@gmail.com>
2022-08-21 14:56:51 +0200

    updated props

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -7,17 +7,16 @@ export type TileType = {
 }
 
 interface TileProps extends TileType {
-    onClick: React.Dispatch<React.SetStateAction<TileType['position'] | null>>
+    onClick: (val: TileType) => void
     debug: boolean
 }
 
 function Tile({ color, position, src, onClick, debug }: TileProps): JSX.Element {
 
-
     return <>
-        <div onClick={() => onClick(src ? position : null)} style={{ left: `${position.x * 7.5}rem`, top: `${position.y * 7.5}rem`, backgroundColor: color } } className={`h-[7.5rem] w-[7.5rem] absolute`}>
+        <div onClick={() => onClick({ color, position, src })} style={{ left: `${position.x * 7.5}rem`, top: `${position.y * 7.5}rem`, backgroundColor: color } } className={`h-[7.5rem] w-[7.5rem] absolute`}>
             <div className='h-full w-full flex justify-center items-center'>
-                {debug && <span className='absolute top-0 w-full'>x: {position.y}<br/> y: {position.x}</span>}{src && <img width={'70%'} src={src} />}
+                {debug && <span className='absolute top-0 w-full'>x: {position.y}<br/> y: {position.x}</span>}{src && <img onDragEnd={() => {}} width={'70%'} className='cursor-pointer' src={src} />}
             </div>
         </div>
     </>
